7e65a44dc7da41044755c21e587a865671cb9d8c
yacnuzun <yacn.uzun@gmail.com>
2025-03-28 17:40:35 +0300

    Delegate Example

---
+++ ConsoleDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ProcessService.ProcessHandler processHandler = new ProcessService.ProcessHandler(ProcessService.Accepted);
+
+processHandler += ProcessService.Send;
+
+processHandler.Invoke();
+
+internal static class ProcessService
+{
+    public delegate void ProcessHandler();
+    private static bool OperationSucess = true;
+    public static void Accepted()
+    {
+        if (OperationSucess != true)
+        {
+            Console.WriteLine("Operation is failed");
+        }
+        else 
+        {
+            Console.WriteLine("Accepted");
+        }
+    }
+
+    public static void Send()
+    {
+        if (!OperationSucess)
+        {
+            Console.WriteLine("Operaiton is failed");
+        }
+        else
+        {
+            Console.WriteLine("Sending");
+        }
+    }
+}
